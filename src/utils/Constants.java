1b52746ae001119eb0dd3e03beff2e6be929d0ce
fwpea <myfriendswood1801@gmail.com>
2024-08-15 16:44:51 -0500

    Added more code

--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -5,4 +5,7 @@ public class Constants {
 	public static final String CONFIGURATION_FILEPATH = 
 			System.getProperty("user.dir") + "/configs/configuration.properties";
 
+	public static final String str = "";
+	
+	
 }
